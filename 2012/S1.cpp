0948e0e73618d10b7b99e3e9b322c9df839a5c6f
Rene <reneleogp0305@gmail.com>
2021-12-16 18:21:34 -0500

    Problems updated

--- 2012/S1.cpp
+++ 2012/S1.cpp
@@ -3,19 +3,19 @@
 using namespace std;
 
 int32_t main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(nullptr);
-	
-	int n;
-	cin >> n;
-	n--; //biggest number always scores, so it's always there
-	/*Answer is to choose 3 out of n-1
-	 * C (n, r) = n! / r!(n-r)!
-	 * C (n, 3) = n! / 3!(n-3)!
-	 * C (n, 3) = n * (n - 1) * (n - 2) / 6
-	 */
-	cout << (n * (n - 1) * (n - 2)) / 6 << endl; // 
-	
-	
-	return 0;
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+    n--;  // biggest number always scores, so it's always there
+    /* Answer is to choose 3 out of n-1
+     * C (n, r) = n! / ( r!(n-r)! )
+     * C (n, 3) = n! / 3!(n-3)!
+     * n! / 3!(n-3)! = n * (n - 1) * (n - 2) / 6
+     * C (n, 3) = n * (n - 1) * (n - 2) / 6
+     */
+    cout << (n * (n - 1) * (n - 2)) / 6 << endl;  //
+
+    return 0;
 }
--- 2013/S2.cpp
+++ 2013/S2.cpp
@@ -3,26 +3,28 @@
 using namespace std;
 
 int32_t main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(nullptr);
-	
-	int w, n;
-	cin >> w >> n;
-	deque<int> cars;
-	int ans = 0, sum = 0;
-	for (int i = 0; i < n; ++i) {
-		int x;
-		cin >> x;
-		sum += x;
-		cars.push_back(x);
-		if (cars.size() > 4) {
-			sum -= cars.front();
-			cars.pop_front();
-		}
-		if (sum > w) break;
-		else ans++;
-	}
-	cout << ans << endl;
-	
-	return 0;
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int w, n;
+    cin >> w >> n;
+    deque<int> cars;
+    int ans = 0, sum = 0;
+    for (int i = 0; i < n; ++i) {
+        int x;
+        cin >> x;
+        sum += x;
+        cars.push_back(x);
+        if (cars.size() > 4) {
+            sum -= cars.front();
+            cars.pop_front();
+        }
+        if (sum > w)
+            break;
+        else
+            ans++;
+    }
+    cout << ans << endl;
+
+    return 0;
 }
